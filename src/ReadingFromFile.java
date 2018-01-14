b1362284183c8f0c285277756f6d3bca112c7664
Alexandra <alexa.zhavnerko@gmail.com>
2018-01-14 14:03:09 +0200

    String lenght count function added

--- src/ReadingFromFile.java
+++ src/ReadingFromFile.java
@@ -1,32 +1,13 @@
 public class ReadingFromFile {
 
 
-        public static void main(StringLenght[] args) throws Exception
-        {
-            // pass the path to the file as a parameter
-           // File file1 =
-           //         new File("C:\\Users\\Kostyantin\\Downloads\\transactionsLog_0.txt");
-          //  Scanner sc1 = new Scanner(file1);
-
-          //  File file2 =
-              //      new File("C:\\Users\\Kostyantin\\Downloads\\transactionsLog_1.txt");
-           // Scanner sc2 = new Scanner(file2);
-
-          //  while (sc1.hasNextLine())
-          //  while (sc2.hasNextLine())
-            //    System.out.println(sc1.nextLine()+sc2.nextLine());
-
-
-
-
-
-
-         
-
-        }
-
-
+    public static void main(String[] args) {
+        String a = "Hello git!";
+        System.out.println(a.length());
     }
+
+
+}
 
 
 
