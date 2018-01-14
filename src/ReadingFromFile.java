0b3d5a373f6548a950ef984f397e889ba5660c4a
Alexandra <alexa.zhavnerko@gmail.com>
2018-01-14 13:58:36 +0200

    String lenght count function added

--- src/ReadingFromFile.java
+++ src/ReadingFromFile.java
@@ -1,27 +1,20 @@
-import java.io.*;
-import java.io.File;
-import java.util.Scanner;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 public class ReadingFromFile {
 
 
-        public static void main(String[] args) throws Exception
+        public static void main(StringLenght[] args) throws Exception
         {
             // pass the path to the file as a parameter
-            File file1 =
-                    new File("C:\\Users\\Kostyantin\\Downloads\\transactionsLog_0.txt");
-            Scanner sc1 = new Scanner(file1);
+           // File file1 =
+           //         new File("C:\\Users\\Kostyantin\\Downloads\\transactionsLog_0.txt");
+          //  Scanner sc1 = new Scanner(file1);
 
-            File file2 =
-                    new File("C:\\Users\\Kostyantin\\Downloads\\transactionsLog_1.txt");
-            Scanner sc2 = new Scanner(file2);
+          //  File file2 =
+              //      new File("C:\\Users\\Kostyantin\\Downloads\\transactionsLog_1.txt");
+           // Scanner sc2 = new Scanner(file2);
 
-            while (sc1.hasNextLine())
-            while (sc2.hasNextLine())
-                System.out.println(sc1.nextLine()+sc2.nextLine());
+          //  while (sc1.hasNextLine())
+          //  while (sc2.hasNextLine())
+            //    System.out.println(sc1.nextLine()+sc2.nextLine());
 
 
 
